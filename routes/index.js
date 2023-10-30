cc0bb33330ba9c7fed5d8962f5ec57251e5cc65c
LamCam <lamcam813@gmail.com>
2023-10-30 13:15:22 +0700

    demo auto search

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var database = require('../database');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'AutoComplete Search in Node.js with MySQL' });
+});
+
+router.get('/get_data', function(request, response, next){
+
+    var search_query = request.query.search_query;
+
+    var query = `
+    SELECT city_name FROM city 
+    WHERE city_name LIKE '%${search_query}%' 
+    LIMIT 10
+    `;
+
+    database.query(query, function(error, data){
+
+        response.json(data);
+
+    });
+
+});
+
+module.exports = router;
